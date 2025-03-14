3983fb353b1826b11b95f76adc73aba9fcdd20a2
ZiadTarek10 <ziadtarekmhamed32@gmail.com>
2025-03-14 16:31:32 +0200

    added the dynamodb.tf file

---
+++ dynamodb.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "dynamodb-table-lottery" {
+  name           = "dynamodb-table-lottery"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "email" # Primary key (hash key)
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+ 
+  tags = {
+    Name        = "dynamodb-table-lottery"
+  }
+}
