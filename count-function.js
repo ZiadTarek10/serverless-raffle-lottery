0f1e97375b51defbbe2bd727ebe432920b19da8d
ZiadTarek10 <ziadtarekmhamed32@gmail.com>
2025-03-17 18:53:20 +0200

    add

--- count-function.js
+++ count-function.js
@@ -1,24 +1,34 @@
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
-const client = new DynamoDBClient({});
+
+const client = new DynamoDBClient({ region: "us-east-1" });
 
 export const handler = async (event, context) => {
     console.log("--devops--start--handler");
     console.log("--devops--event", event);
+    
     let TableName = "dynamodb-table-lottery";
+    
     try {
         const command = new ScanCommand({
             TableName: TableName,
-            Select : "COUNT"
+            Select: "COUNT"
         });
-    
+
         const result = await client.send(command);
 
         console.log("--devops--result", result);
-        return result.Count;
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ count: result.Count }) 
+        };
+
     } catch (e) {
-        console.log("--devops--error", e);
-        return e.message;
+        console.error("--devops--error", e);
+        
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error scanning table", error: e.message })
+        };
     }
 };
-
-    
